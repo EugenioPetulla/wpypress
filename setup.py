dbc10232f5b6252ff2b3b11bdc564f00b6c17e5a
Eugenio Petullà <eugeniopetulla@yahoo.it>
2025-05-24 22:54:13 +0200

    Rename project for pypi

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="wp_api",
+    name="wpypress",
     version="0.1.0",
     author="Eugenio Petullà",
     author_email="eugenio@codeat.it",
     description="A versatile Python library for seamless interaction with the WordPress REST API, including built-in SEO features and more.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/EugenioPetulla/wp_api",
+    url="https://github.com/EugenioPetulla/wpypress",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
